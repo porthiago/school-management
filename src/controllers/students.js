f8e937d189880f5f159e5e3df4fd15d8b1884332
Thiago Porto <porthiago@gmail.com>
2022-08-15 15:56:03 -0300

    inProgress: select by id or name

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -2,7 +2,7 @@ const express = require('express');
 const knex = require('../config/connection');
 
 const index = async (req, res) => {
-  const { serie, situacao, modalidade } = req.query;
+  const { serie, situacao, modalidade, nome, id } = req.query;
 
   try {
     if (serie) {
@@ -33,8 +33,7 @@ const index = async (req, res) => {
       return res.json({ alunos: students, total: students.length });
     }
 
-    if (modalidade && !serie) { 
-      
+    if (modalidade && !serie) {
       if (situacao) {
         const students = await knex('alunos')
           .whereILike('modalidade', modalidade)
@@ -50,17 +49,17 @@ const index = async (req, res) => {
         return res.json({ alunos: students, total: students.length });
       }
 
-        const students = await knex('alunos')
-          .whereILike('modalidade', modalidade)
-          .select('*')
-          .orderBy('serie')
-          .orderBy('nome');
-        if (students.length === 0) {
-          return res.status(404).json({ message: 'Nenhum aluno encontrado' });
-        }
-  
-        return res.json({ alunos: students, total: students.length });
-    } 
+      const students = await knex('alunos')
+        .whereILike('modalidade', modalidade)
+        .select('*')
+        .orderBy('serie')
+        .orderBy('nome');
+      if (students.length === 0) {
+        return res.status(404).json({ message: 'Nenhum aluno encontrado' });
+      }
+
+      return res.json({ alunos: students, total: students.length });
+    }
 
     if (situacao) {
       const students = await knex('alunos')
@@ -84,9 +83,34 @@ const index = async (req, res) => {
       return res.status(404).json({ message: 'Nenhum aluno encontrado' });
     }
 
+    if (nome || id) {
+      const students = await knex('alunos')
+        .whereILike('nome', nome)
+        .select('*')
+        .orderBy('serie')
+        .orderBy('nome');
+      if (students.length === 0) {
+        return res.status(404).json({ message: 'Nenhum aluno encontrado' });
+      }
+      console.log(id);
+
+      if (id) {
+        const students = await knex('alunos')
+          .where('id', id)
+          .select('*')
+          .orderBy('serie')
+          .orderBy('nome');
+        if (students.length === 0) {
+          return res.status(404).json({ message: 'Nenhum aluno encontrado' });
+        }
+      }
+
+      return res.json({ alunos: students, total: students.length });
+    }
+
     return res.json({ alunos: students, total: students.length });
   } catch (error) {
-    return res.status(500).json({ message: error });
+    return res.status(500).json({ message: error.message });
   }
 };
 
