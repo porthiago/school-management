2286f24f0991ff593491166896e00e6c7b4e6624
Thiago Porto <porthiago@gmail.com>
2022-08-16 16:33:29 -0300

    update: for functions of recently added new features, routes and validation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const studentsRoutes = require('./routes/studentsRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRoutes);
+app.use('/admin', adminRoutes);
 app.use('/aluno', studentsRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
