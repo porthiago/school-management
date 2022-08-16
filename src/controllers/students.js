b81a8778da7e8a512aec7d06de2804e19c5f02b0
Thiago Porto <porthiago@gmail.com>
2022-08-16 16:29:17 -0300

    feature: function to insert a new student into database

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const knex = require('../config/connection');
+const { studentsSchema } = require('../validations/studentsSchema');
 
 const index = async (req, res) => {
   const { serie, situacao, modalidade, nome, id } = req.query;
@@ -117,11 +118,26 @@ const index = async (req, res) => {
 const create = async (req, res) => {
   const { nome, situacao, observacao, serie, turma, modalidade } = req.body;
 
-  const knexInsert = await knex('alunos')
-    .insert({ nome, situacao, observacao, serie, turma, modalidade })
-    .returning('*');
+  try {
+    await studentsSchema.validate(req.body);
 
-  return res.json(knexInsert);
+    const studentData = await knex('alunos')
+      .insert({ nome, situacao, observacao, serie, turma, modalidade })
+      .returning('*');
+
+    if (studentData.length === 0) {
+      return res.status(400).json({
+        message: 'Ocorreu um erro ao inserir o aluno no banco de dados.'
+      });
+    }
+
+    return res.json({
+      messagem: 'Aluno Cadastrado com Sucesso.',
+      aluno: studentData[0]
+    });
+  } catch (error) {
+    return res.status(500).json({ messagem: error.message });
+  }
 };
 
 module.exports = {
